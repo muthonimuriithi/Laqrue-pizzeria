9ac75048c3ebce7e18df5aeaba2c766a13bbbd51
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-03-21 08:15:50 +0300

    modified functionality of the page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+// $(document).ready(function(){
 
-    
-}
-
-)
+//     var a=parseInt($("#size").val());
+//     var b=parseInt($("#toppings").val());
+//     var c=parseInt($("#crust").val());
+//     var sum=a+b+c;
+//     alert($(sum));
+// })
