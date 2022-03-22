7d603c8bb5f2db4c4b0d126ef90965e6eb3b4016
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-03-22 23:45:46 +0300

    fixed a bug on buttons functionality of not  displaying total and making another order

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-$(document).ready(function(){
+console.log("loise")
+$(document).ready(function () {
 
-    $("table").hide();
-    $(".high").hide();
-    $(".high1").hide();
-    $(".yes").hide();
-    $(".no").hide();
-    $(".high1 h4").hide();
-    
-    $(".order").click(function(e) {
+  $("table").hide();
+  $(".high").hide();
+  $(".high1").hide();
+  $(".yes").hide();
+  $(".no").hide();
+  $(".high1 h4").hide();
+
+  $(".order").click(function () {
+    var sizeOfPizza = $(".size option:selected").val();
+    var pizzaToppings = $(".toppings option:selected").val();
+    var pizzaCrust = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+    var order = 1;
+    var maxTotal = 0;
+
+    // e.preventDefault()
+
+
+
+
+    $("table").show();
+    $(".high").show();
+    $(".order").hide();
+
+
+    $("#sizes").html($(".size option:selected").text() + " - " + sizeOfPizza);
+    $("#topping").html($(".toppings option:selected").text() + " - " + pizzaToppings);
+    $("#crusts").html($(".crust option:selected").text() + " - " + pizzaCrust);
+    $("#total").html(total);
+
+    function myPizza(size, toppings, crust, total, orderNo) {
+      this.size = size;
+      this.toppings = toppings;
+      this.crust = crust;
+      this.total = total;
+      this.orderNo = orderNo;
+    }
+
+
+    $('.butt').click(function () {
       var sizeOfPizza = $(".size option:selected").val();
       var pizzaToppings = $(".toppings option:selected").val();
       var pizzaCrust = $(".crust option:selected").val();
-      var total= parseInt(sizeOfPizza) + parseInt(pizzaToppings) + parseInt(pizzaCrust); 
-      var order = 1;
-      var maxTotal = 0;
-
-      e.preventDefault()
-  
-    
-      
-  
-      $("table").show();
-      $(".high").show();
-      $(".order").hide();
-      
-})
-$("#sizes").html($(".size option:selected").text() + " - " + sizeOfPIzza);
-$("#topping").html($(".toppings option:selected").text() + " - " + pizzaToppings);
-$("#crusts").html($(".crust option:selected").text() + " - " + pizzaCrust);
-$("#total").html(total);
-
-function myPizza(size, toppings, crust, total, orderNo) {
-  this.size = size;
-  this.toppings = toppings;
-  this.crust = crust;
-  this.total = total;
-  this.orderNo = orderNo;
-}
+      var total = parseInt(sizeOfPizza) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+      order = order + 1;
+      maxTotal = maxTotal + total;
+      console.log("maxTotal")
 
 
-$('.butt').click(function() {
-  var sizeOfPizza = $(".size option:selected").val();
-  var pizzaToppings= $(".toppings option:selected").val();
-  var pizzaCrust = $(".crust option:selected").val();
-  var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-  order = order + 1;
-  maxTotal = maxTotal + total;
+      var newPizza = new myPizza(sizeOfPizza, pizzaToppings, pizzaCrust, total, order);
 
+      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
 
-  var newPizza = new myPizza(sizeOfPizza, pizzaToppings, pizzaCrust, total, order);
-
-  var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
-
-  $("#pizza").append(newRow);
+      $("#pizza").append(newRow);
+    });
+  });
 });
-})
